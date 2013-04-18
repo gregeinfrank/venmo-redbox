509415cf57318b125aa8b9b1baf73a4bf6181d75
Gregory Einfrank <greg.einfrank@venmo.com>
2013-04-18 14:51:22 -0400

    adding help text

--- lib/constants.py
+++ lib/constants.py
@@ -1,3 +1,15 @@
 DB_NAME = 'venmo_redbox'
 DB_USER = 'root'
 DB_PASSWORD = 'venmo'
+
+HELP_TEXT = """
+usage: venmo_redbox.py <who|rent|steal> <box-name>
+
+who:  Displays who currently owns box-name, if it is taken.
+
+rent: Rents the box if it is available ( also gives option to steal )
+
+steal: Rents the box even if it is not available.  Don't be a jerk!  Limit 
+       the usage of 'steal'
+
+"""
