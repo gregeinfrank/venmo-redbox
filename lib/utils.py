b630a8883596b4c9d079eafa397ec45737f6b12f
Gregory Einfrank <greg.einfrank@venmo.com>
2013-04-18 14:09:58 -0400

    save some good fonts

--- lib/utils.py
+++ lib/utils.py
@@ -1,8 +1,26 @@
 import urllib2
 from random import choice
 
+ascii_fonts = ['isometric4',
+               'starwars',
+               'clr8x8',
+               'alligator',
+               'char3___',
+               'ticks',
+               'tombstone',
+               'p_s_h_m_',
+               'colossal',
+               'shadow',
+               'ascii___',
+               'drpepper',
+               'basic',
+               'rounded',
+               'nancyj',
+               'xhelvi',
+               'e__fist_',
+               'chunky']
+
 def ascii_print(string):
-    ascii_fonts = urllib2.urlopen("http://artii.herokuapp.com/fonts_list").read().split('\n')
     font = choice(ascii_fonts)
     print font
     print urllib2.urlopen("http://artii.herokuapp.com/make?text=%s&font=%s" % (string, font)).read()
